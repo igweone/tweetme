043534dc7696154b7f5d3ffb2afef8512d8f0f32
Emeka Osigwe <emeka@intelligenti.io>
2019-03-11 17:27:59 +0100

    setup static files and urls configuration

--- tweetme/settings/local.py
+++ tweetme/settings/local.py
@@ -117,4 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/some-static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static-storage"),
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-serve")
